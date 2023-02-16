40f89756a91299db6a8fdd88fe60d32ef2c3d6c0
bluetooth-afk <58256651+bluetooth-afk@users.noreply.github.com>
2023-02-16 12:35:07 +0100

    randomplayer is random

--- Barbajuan/Players/RandomPlayer.cs
+++ Barbajuan/Players/RandomPlayer.cs
@@ -4,4 +4,16 @@ public class RandomPlayer : Player
     {
         this.hand = hand;
     }
+
+    public new Card action(IgameState gameState)
+    {
+        var moves = getActions(gameState);
+        if (moves.Count() == 0) return new Card(CardColor.WILD,CardType.DRAW1);
+
+        var random = new Random();
+
+        var randomMove = random.Next(moves.Count());
+
+        return moves[randomMove];
+    }
 }
