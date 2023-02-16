ce3c01e99e3274213463769b82fc6cce563f26e2
tkjelds <tkjelds385@gmail.com>
2023-02-16 13:39:55 +0100

    format

--- Barbajuan/Card/CardExtensions.cs
+++ Barbajuan/Card/CardExtensions.cs
@@ -3,7 +3,8 @@ public static class CardExtensions
 {
     public static bool canBePlayedOn(this Card card1, Card card2)
     {
-        if (card2.cardColor == WILD || card1.cardColor == WILD) return true;
+        if (card2.cardColor == CardColor.WILD || card1.cardColor == CardColor.WILD) return true;
+        if (card2.cardType == CardType.DRAW4 || card2.cardType == CardType.SELECTCOLOR) return true;
         if (card2.cardColor == card1.cardColor) return true;
         if (card2.cardType == card1.cardType) return true;
         //TODO
