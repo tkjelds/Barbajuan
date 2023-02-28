bc4c2e52f62016d6f3dce5d98467c9a219f32f13
tkjelds <tkjelds385@gmail.com>
2023-02-28 14:42:30 +0100

    added csv content to AverageNumberOfTurns.csv

--- Barbajuan/Program.cs
+++ Barbajuan/Program.cs
@@ -2,28 +2,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        List<List<Iplayer>> scoreBoards = new List<List<Iplayer>>();
-        List<(String, List<int>)> playerPlacements = new List<(string, List<int>)>();
-        playerPlacements.Add(("bot 1", new List<int>() { 0, 0, 0, 0 }));
-        playerPlacements.Add(("bot 2", new List<int>() { 0, 0, 0, 0 }));
-        playerPlacements.Add(("bot 3", new List<int>() { 0, 0, 0, 0 }));
-        playerPlacements.Add(("bot 4", new List<int>() { 0, 0, 0, 0 }));
-        // using (var writer = new StreamWriter(@".\Documentation\AverageNumberOfTurns.csv"))
-        // using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture)){
-        for (int i = 0; i < 1000; i++)
-        {
-            var players = new List<Iplayer>(){
-                new StackingMovePlayer("bot 1"),
-                new StackingMovePlayer("bot 2"),
-                new StackingMovePlayer("bot 3"),
-                new StackingMovePlayer("bot 4")
-            };
-            var gameState = new GameState(players);
-            var nmbrOfTurns = gameState.runReturnNumberOfTurns();
-            //csv.WriteRecord(nmbrOfTurns);
-            
-        }
-       // }
+        // List<List<Iplayer>> scoreBoards = new List<List<Iplayer>>();
+        // List<(String, List<int>)> playerPlacements = new List<(string, List<int>)>();
+        // playerPlacements.Add(("bot 1", new List<int>() { 0, 0, 0, 0 }));
+        // playerPlacements.Add(("bot 2", new List<int>() { 0, 0, 0, 0 }));
+        // playerPlacements.Add(("bot 3", new List<int>() { 0, 0, 0, 0 }));
+        // playerPlacements.Add(("bot 4", new List<int>() { 0, 0, 0, 0 }));
         // Parallel.For(1, 100000, number =>
         // {
         //     var players = new List<Iplayer>(){
